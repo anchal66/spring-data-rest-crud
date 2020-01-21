ca6f64203cb4fff93b0b352ea49be6e38ffd63ee
Avinash Anchal <you@example.comanchalavinash66@gmail.com>
2020-01-21 06:53:09 +0530

    Added /magic-api/.. and /magic-api/members

--- src/main/java/com/anchal/springdemoproj/dao/EmployeeRepository.java
+++ src/main/java/com/anchal/springdemoproj/dao/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.anchal.springdemoproj.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.anchal.springdemoproj.entity.Employee;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
